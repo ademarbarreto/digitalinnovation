5b3775a5e461e9a35bff039ef511eb381f361cc4
ademarbarreto <ademar.barreto@globo.com>
2021-07-08 15:04:55 -0300

    Created account class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Conta.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.collections;
+
+import java.math.BigDecimal;
+
+public class Conta (
+    val agencia: String
+    val numero: String
+    val saldo: BigDecimal
+) {
+    fun deposito (valor: BigDecimal) {
+
+    }
+
+    fun saque (valor: BigDecimal) {
+
+    }
+}
