ef6c7b6116781240be095bf415270567f7429638
ademarbarreto <ademar.barreto@globo.com>
2021-07-08 16:55:54 -0300

    Created Banco.kt Created BancoTeste.kt

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testes/BancoTeste.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.collections.testes
+
+import one.digitalinnovation.collections.Banco
+
+fun main() {
+    val digiOneBank = Banco(nome="digiOne", numero= 12)
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+}
