9fcd0782e0a112d40bc63bac4e8e502ac777bfdd
ademarbarreto <ademar.barreto@globo.com>
2021-07-08 22:33:47 -0300

    created enum TesteBanco.kt TesteFuncionario.kt

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testes/TestePessoa.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.collections.testes
+
+import one.digitalinnovation.collections.Pessoa
+
+fun main()  {
+    val jether = Pessoa(nome = "Jether Rodrigues", cpf = "1234567899")
+    println(jether.nome)
+    println(jether.cpf)
+
+}
