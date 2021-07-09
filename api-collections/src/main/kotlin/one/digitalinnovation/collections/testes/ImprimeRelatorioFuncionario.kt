a20f426760271c7aad22558253f84b693c5c8693
ademarbarreto <ademar.barreto@globo.com>
2021-07-09 14:37:39 -0300

    created ImprimeRelatorioFuncionario.kt

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/testes/ImprimeRelatorioFuncionario.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.collections.testes
+
+import one.digitalinnovation.collections.Funcionario
+
+class ImprimeRelatorioFuncionario {
+    companion object {
+        fun imprime(funcionario: Funcionario) {
+            println(funcionario.toString())
+        }
+    }
+}
